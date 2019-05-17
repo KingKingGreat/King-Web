9ab8ebd53cd534321d3894c385892601b801438c
King <47883824+KingKingGreat@users.noreply.github.com>
2019-05-17 20:57:25 +0700

    Update JS
    Fixed Invisible <p> & <a> Bug

--- King Website/script.js
+++ King Website/script.js
@@ -39,16 +39,30 @@ var fade;
 titles[0].addEventListener("click", function() {
 	if (descs[0].style.opacity < 1) {
 		fade = window.setInterval(function() {animate("descAbout", true)}, 50);
+		descs[0].style["pointer-events"] = "auto";
 	} else {
 		fade = window.setInterval(function() {animate("descAbout", false)}, 50);
+		descs[0].style["pointer-events"] = "none";
 	}
 }); // About
 
 titles[1].addEventListener("click", function() {
 	if (descs[1].style.opacity < 1) {
 		fade = window.setInterval(function() {animate("descProducts", true)}, 50);
+		descs[1].style['pointer-events'] = "auto";
+		var link = document.querySelectorAll(".links");
+
+		for (var i = 0; i < link.length; i ++) {
+			link[i].style["pointer-events"] = "auto";
+		}
 	} else {
 		fade = window.setInterval(function() {animate("descProducts", false)}, 50);
+		descs[1].style['pointer-events'] = "none";
+		var link = document.querySelectorAll(".links");
+
+		for (var i = 0; i < link.length; i ++) {
+			link[i].style["pointer-events"] = "none";
+		}
 	}
 }); // Products
 
@@ -59,3 +73,15 @@ descs[1].style.opacity = 0; // Products
 
 // Fade page
 fade = window.setInterval(function() {animate("body", true)}, 100);
+
+descs[0].style['pointer-events'] = "none";
+descs[1].style['pointer-events'] = "none";
+var link = document.querySelectorAll(".links");
+
+for (var i = 0; i < link.length; i ++) {
+	link[i].style["pointer-events"] = "none";
+}
+
+var d = new Date();
+var n = d.getFullYear();
+document.getElementById("version").innerHTML = "<strong>Version 1.0.2019517.171601.LM</strong><br><strong>© " + n + " KingKingGreat</strong>"
