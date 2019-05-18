02a20ec68fd8002b6f4609e2eb7e631b65197035
King <47883824+KingKingGreat@users.noreply.github.com>
2019-05-18 09:52:37 +0700

    Updated JS
    Added  event for phone users

--- King Website/script.js
+++ King Website/script.js
@@ -36,33 +36,38 @@ var titles = [
 var fade;
 
 // Add event listner to Titles
+
+// Click Event (Laptop/PC users)
 titles[0].addEventListener("click", function() {
 	if (descs[0].style.opacity < 1) {
 		fade = window.setInterval(function() {animate("descAbout", true)}, 50);
-		descs[0].style["pointer-events"] = "auto";
 	} else {
 		fade = window.setInterval(function() {animate("descAbout", false)}, 50);
-		descs[0].style["pointer-events"] = "none";
 	}
 }); // About
 
 titles[1].addEventListener("click", function() {
 	if (descs[1].style.opacity < 1) {
 		fade = window.setInterval(function() {animate("descProducts", true)}, 50);
-		descs[1].style['pointer-events'] = "auto";
-		var link = document.querySelectorAll(".links");
-
-		for (var i = 0; i < link.length; i ++) {
-			link[i].style["pointer-events"] = "auto";
-		}
 	} else {
 		fade = window.setInterval(function() {animate("descProducts", false)}, 50);
-		descs[1].style['pointer-events'] = "none";
-		var link = document.querySelectorAll(".links");
+	}
+}); // Products
 
-		for (var i = 0; i < link.length; i ++) {
-			link[i].style["pointer-events"] = "none";
-		}
+// Tap Event (Ipad, Tablets, Phone Users)
+titles[0].addEventListener("touchstart", function(ev) {
+	if (descs[0].style.opacity < 1) {
+		fade = window.setInterval(function() {animate("descAbout", true)}, 50);
+	} else {
+		fade = window.setInterval(function() {animate("descAbout", false)}, 50);
+	}
+}); // About
+
+titles[1].addEventListener("touchstart", function() {
+	if (descs[1].style.opacity < 1) {
+		fade = window.setInterval(function() {animate("descProducts", true)}, 50);
+	} else {
+		fade = window.setInterval(function() {animate("descProducts", false)}, 50);
 	}
 }); // Products
 
@@ -73,15 +78,3 @@ descs[1].style.opacity = 0; // Products
 
 // Fade page
 fade = window.setInterval(function() {animate("body", true)}, 100);
-
-descs[0].style['pointer-events'] = "none";
-descs[1].style['pointer-events'] = "none";
-var link = document.querySelectorAll(".links");
-
-for (var i = 0; i < link.length; i ++) {
-	link[i].style["pointer-events"] = "none";
-}
-
-var d = new Date();
-var n = d.getFullYear();
-document.getElementById("version").innerHTML = "<strong>Version 1.0.2019517.171601.LM</strong><br><strong>© " + n + " KingKingGreat</strong>"
