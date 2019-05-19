4cc2b55197eab48fec80baca85bc3c998eeb18da
King <47883824+KingKingGreat@users.noreply.github.com>
2019-05-19 11:46:22 +0700

    Update JS
    Fixed Bugs & Glitches

--- King Website/script.js
+++ King Website/script.js
@@ -1,8 +1,12 @@
-// Animation
+// Animation Function
 function animate(id, v) {
+	// Connect Parameters
 	var element = document.getElementById(id);
 	var currentOpacity = parseFloat(element.style.opacity);
+
+	// Check for true & false
 	if (v) {
+		// Fade In
 		if (currentOpacity < 1) {
 		    element.style.opacity = currentOpacity + 0.1;   
 		    currentOpacity = parseFloat(element.style.opacity);
@@ -10,6 +14,7 @@ function animate(id, v) {
 		    window.clearInterval(fade);
 		}	    
 	} else {
+		// Fade Out
 		if (currentOpacity > 0) {
 		    element.style.opacity = currentOpacity - 0.1;   
 		    currentOpacity = parseFloat(element.style.opacity);
@@ -33,23 +38,29 @@ var titles = [
 	document.getElementById("titleProducts") // Products
 ];
 
+// Declare Animation Variable
 var fade;
 
 // Add event listner to Titles
 
 // Click Event (Laptop/PC users)
 titles[0].addEventListener("click", function() {
+	// Checker Desc Opacity
 	if (descs[0].style.opacity < 1) {
+		// Fade In
 		fade = window.setInterval(function() {animate("descAbout", true)}, 50);
 		descs[0].style["pointer-events"] = "auto";
 	} else {
+		// Fade Out
 		fade = window.setInterval(function() {animate("descAbout", false)}, 50);
 		descs[0].style["pointer-events"] = "none";
 	}
 }); // About
 
 titles[1].addEventListener("click", function() {
+	// Checker Desc Opacity
 	if (descs[1].style.opacity < 1) {
+		// Fade In
 		fade = window.setInterval(function() {animate("descProducts", true)}, 50);
 		descs[1].style['pointer-events'] = "auto";
 		var link = document.querySelectorAll(".links");
@@ -58,6 +69,7 @@ titles[1].addEventListener("click", function() {
 			link[i].style["pointer-events"] = "auto";
 		}
 	} else {
+		// Fade Out
 		fade = window.setInterval(function() {animate("descProducts", false)}, 50);
 		descs[1].style['pointer-events'] = "none";
 		var link = document.querySelectorAll(".links");
@@ -70,17 +82,22 @@ titles[1].addEventListener("click", function() {
 
 // Tap Event (Ipad, Tablets, Phone Users)
 titles[0].addEventListener("touchstart", function(ev) {
+	// Checker Desc Opacity
 	if (descs[0].style.opacity < 1) {
+		// Fade In
 		fade = window.setInterval(function() {animate("descAbout", true)}, 50);
 		descs[0].style["pointer-events"] = "auto";
 	} else {
+		// Fade Out
 		fade = window.setInterval(function() {animate("descAbout", false)}, 50);
 		descs[0].style["pointer-events"] = "none";
 	}
 }); // About
 
 titles[1].addEventListener("touchstart", function() {
+	// Checker Desc Opacity
 	if (descs[1].style.opacity < 1) {
+		// Fade In
 		fade = window.setInterval(function() {animate("descProducts", true)}, 50);
 		descs[1].style['pointer-events'] = "auto";
 		var link = document.querySelectorAll(".links");
@@ -89,6 +106,7 @@ titles[1].addEventListener("touchstart", function() {
 			link[i].style["pointer-events"] = "auto";
 		}
 	} else {
+		// Fade Out
 		fade = window.setInterval(function() {animate("descProducts", false)}, 50);
 		descs[1].style['pointer-events'] = "none";
 		var link = document.querySelectorAll(".links");
@@ -99,14 +117,16 @@ titles[1].addEventListener("touchstart", function() {
 	}
 }); // Products
 
+// On Start Up
+
 // Hide desc elements
 document.getElementById("body").style.opacity = 0;
 descs[0].style.opacity = 0; // About
 descs[1].style.opacity = 0; // Products
 
-// Fade page
-fade = window.setInterval(function() {animate("body", true)}, 100);
-
-// Version
+// Display Version
 var d = new Date();
-document.getElementById("version").innerHTML = "<strong>Version 1.2.2019518.175601.LM</strong><br>© " + d.getFullYear() + " KingKingGreat";
+document.getElementById("version").innerHTML = "<strong>Version 1.3.2019519.113801.LM</strong><br><strong>© " + d.getFullYear() + " KingKingGreat</strong>";
+
+// Fade in page
+fade = window.setInterval(function() {animate("body", true)}, 100);
